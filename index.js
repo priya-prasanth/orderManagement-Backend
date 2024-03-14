cd153e4e44db037886cb32c64a5a48f96ef98b49
priya-prasanth <priyaprasanths168.com>
2024-03-14 15:28:51 +0530

    Changes made

--- index.js
+++ index.js
@@ -10,41 +10,36 @@ import productRoute from "./Routes/ProductRoutes.js";
 import userRouter from "./Routes/UserRoutes.js";
 import orderRouter from "./Routes/orderRoutes.js";
 
-
-
-
 const app = express();
 app.use(cors());
 app.use(express.json());
 
- app.use(
-   cors({
-     origin: ["http://localhost:5173"],
-      methods: ["GET", "POST"],
-     credentials: true,
-   })
+app.use(
+  cors({
+    origin: ["https://shiny-kelpie-c61860.netlify.app"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
 );
- 
+
 app.use(cookieParser());
 
 connectDB();
 
-    //   app.get("/api/products", (req, res) => {
-    //     res.json(products);
-    //   })
+//   app.get("/api/products", (req, res) => {
+//     res.json(products);
+//   })
 
-    // app.get("/api/products/:id", (req, res) => {
-    //   const product = products.find((p) => p._id === req.params.id);
-    //    res.json(product);
-    //  });
-  
+// app.get("/api/products/:id", (req, res) => {
+//   const product = products.find((p) => p._id === req.params.id);
+//    res.json(product);
+//  });
+
 // API
 app.use("/api/import", ImportData);
 app.use("/api/products", productRoute);
 app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
-
-
 
 app.get("/", (req, res) => {
   res.send("API is running...");
@@ -54,7 +49,3 @@ const port = process.env.PORT;
 app.listen(port, () => {
   console.log("My app is listening at-", port);
 });
-
-
-
-
